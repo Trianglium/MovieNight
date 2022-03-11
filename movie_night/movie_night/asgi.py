223eb98f201a03f961863737ea7c2ce6801c6bf4
Trianglium <jbrockerrudow@gmail.com>
2022-03-11 09:34:11 -0600

    Fix filenames mistake

--- movie_night/movie_night/asgi.py
+++ movie_night/movie_night/asgi.py
@@ -1,18 +1,7 @@
-"""movie_night URL Configuration
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-"""
-from django.contrib import admin
-from django.urls import path, include
+import os
 
-from django.conf import settings
-from django.conf.urls.static import static
+from django.core.asgi import get_asgi_application
 
-from static import *
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "movie_night.settings")
 
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+application = get_asgi_application()
