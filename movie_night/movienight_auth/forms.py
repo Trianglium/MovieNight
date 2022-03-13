8a0f02557b77658404f602b4ae7f6a8eec1c7c12
Trianglium <jbrockerrudow@gmail.com>
2022-03-13 09:48:26 -0500

    Registration Form finished

--- movie_night/movienight_auth/forms.py
+++ movie_night/movienight_auth/forms.py
@@ -1,5 +1,14 @@
 from django_registration.forms import RegistrationForm as DefaultRegistrationForm
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
+from movienight_auth.models import User
 
 class RegistrationForm(DefaultRegistrationForm):
-    pass
+    class Meta(RegistrationForm.Meta):
+        model = User
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit("submit", "Register"))
