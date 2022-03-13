1190a2a729f7d1bb2333aebd9385c55385955edf
Trianglium <jbrockerrudow@gmail.com>
2022-03-13 09:49:11 -0500

    Accounts url routing

--- movie_night/movie_night/urls.py
+++ movie_night/movie_night/urls.py
@@ -1,18 +1,43 @@
-"""movie_night URL Configuration
+"""movienight URL Configuration
+
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
 from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path, include
+from django_registration.backends.activation.views import RegistrationView
 
-from static import *
-
+import movienight_auth.views
+import movies.views
+from movienight_auth.forms import RegistrationForm
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("accounts/profile/", movienight_auth.views.profile, name="profile"),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path(
+        "accounts/register/",
+        RegistrationView.as_view(form_class=RegistrationForm),
+        name="django_registration_register",
+    ),
+    path("", movies.views.index),
+    path("movie-nights/", movies.views.movie_night_list, name="movie_night_list_ui"),
+    path(
+        "movie-nights/<int:pk>/",
+        movies.views.movie_night_detail,
+        name="movie_night_detail_ui",
+    ),
+    path("search/", movies.views.movie_search, name="movie_search_ui"),
+    path("movies/<slug:imdb_id>/", movies.views.movie_detail, name="movie_detail_ui"),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
