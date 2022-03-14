2f6b70d0978cd0be03ef4a230a9ea7f73a884d3e
Trianglium <jbrockerrudow@gmail.com>
2022-03-14 08:23:34 -0500

    DRF Config: auth, permissions, pagination, jwt

--- movie_night/movie_night/settings.py
+++ movie_night/movie_night/settings.py
@@ -191,6 +191,20 @@ class Dev(Configuration):
         },
     }
 
+    REST_FRAMEWORK = {
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+            "rest_framework_simplejwt.authentication.JWTAuthentication",
+        ]
+        "DEFAULT_PERMISSION_CLASSES": [
+            "rest_framework.permissions.IsAuthenticated"
+        ],
+        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+        "PAGE_SIZE": 100,
+    }
+
 class Prod(Dev):
     DEBUG = False
     SECRET_KEY = values.SecretValue()
